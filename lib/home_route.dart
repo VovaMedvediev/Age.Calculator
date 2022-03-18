0e2ba75de605f4e46236b69eb8fdaa9aedddc822
Yaroslav <yarshaqa@gmail.com>
2022-03-18 20:39:36 +0200

    Architecture Setup (Bloc)

---
+++ lib/home_route.dart
@@ -0,0 +1,15 @@
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
+//
+// import 'package:flutter/material.dart';
+//
+// ...
+//
+// class _HomeRouteState extends State<HomeRoute> {
+//
+//   ...
+//
+//   Future<InitializationStatus> _initGoogleMobileAds() {
+//   // TODO: Initialize Google Mobile Ads SDK
+//   return MobileAds.instance.initialize();
+//   }
+// }
