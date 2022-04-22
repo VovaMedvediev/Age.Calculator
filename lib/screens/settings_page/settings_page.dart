e2c71948680d7c601b2092b04fc42db6c7e7904f
Yaroslav <yarshaqa@gmail.com>
2022-04-22 20:12:34 +0300

    deleted odd files

--- lib/screens/settings_page/settings_page.dart
+++ lib/screens/settings_page/settings_page.dart
@@ -2,7 +2,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:permission_handler/permission_handler.dart';
-
 import 'settings.dart';
 
 class SettingsPage extends StatefulWidget {
